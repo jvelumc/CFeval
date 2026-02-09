a14d3c58d6544b5c50699a3f38710fa9922518ea
Jasper van Egeraat (PHEG) <j.w.a.van_egeraat@lumc.nl>
2026-02-09 18:49:48 +0100

    why auc of naive and causal model same

--- dev/scratchpad.R
+++ dev/scratchpad.R
@@ -1,91 +1,30 @@
-set.seed(1)
-n <- 5000
-data <- data.frame(
-  L1 = rnorm(n, mean = 0),
-  L2 = rbinom(n, 1, 0.5),
-  P1 = rnorm(n, mean = 0),
-  P2 = rnorm(n, mean = 0)
-)
-data$A <- rbinom(n, 1, plogis(0.9*data$L1 + 0.4*data$L2))
-data$Y0 <- rbinom(n, 1, plogis(0.1 + 0.9*data$L1 + 0.6*data$L2 + 0.4*data$P1 + 0.3*data$P2))
-data$Y1 <- rbinom(n, 1, plogis(0.1 + 0.9*data$L2 + 0.6*data$L2 + 0.4*data$P1 + 0.3*data$P2 - 0.8))
-data$Y <- ifelse(data$A == 1, data$Y1, data$Y0)
-
-summary(data$A)
-
-model0 <- suppressWarnings(
-  glm(
-    Y ~ A + P1,
-    family = "binomial",
-    data = data
-  )
-)
-
-model1 <- suppressWarnings(
-  glm(
-    Y ~ A + P1 + P2,
-    family = "binomial",
-    data = data
-  )
-)
-model2 <- suppressWarnings(
-  glm(
-    Y ~ A + P1 + P2,
-    family = "binomial",
-    data = data,
-    weights = ipt_weights(data, A ~ L1 + L2)$weights
-  )
-)
-
-print_model(model2)
-
-cfscore <- CFscore(
-  data = data,
-  object = list("lol" = model0, "hi" = model1, "hello" = model2),
-  outcome_formula = Y ~ 1,
-  treatment_formula = A ~ L1 + L2,
-  treatment_of_interest = 0,
-  bootstrap = 100
-)
-
-
-cfscore
-cfscore$ipt$weights |> summary()
-
-cfscore_s <- CFscore(
-  data = data,
-  object = list(model0, model1, model2),
-  outcome_formula = Y ~ 1,
-  treatment_formula = A ~ L1 + L2,
-  treatment_of_interest = 0,
-  bootstrap = 200,
-  stable_iptw = TRUE
-)
-
-cfscore_s$ipt$weights |> summary()
-cfscore$score$auc
-cfscore_s
-
-plot(x = cfscore$bootstrap[[1]]$calplot[[1]],
-     y = cfscore$bootstrap[[1]]$calplot[[2]],
-     type = "l", xlab = "observed", ylab = "predicted",xlim = c(0,1), ylim = c(0,1))
-
-for (i in 2:length(cfscore$bootstrap)) {
-  lines(
-    x = cfscore$bootstrap[[i]]$calplot[[1]],
-    y = cfscore$bootstrap[[i]]$calplot[[2]],
-    type = "l"
-  )
+simulate_data <- function(n) {
+  data <- data.frame(id = 1:n)
+  data$L <- rnorm(n)
+  data$A <- rbinom(n, 1, plogis(2*data$L))
+  data$P1 <- rnorm(n)
+  data$P2 <- rbinom(n,1, plogis(data$L))
+  data$Y0 <- rbinom(n, 1, plogis(0.5 + 2*data$L + 1.25 * data$P1 + 0.8 * data$P2))
+  data$Y1 <- rbinom(n, 1, plogis(0.5 + 2*data$L + 1.25 * data$P1 + 0.8 * data$P2 - 0.6))
+  data$Y <- ifelse(data$A == 0, data$Y0, data$Y1)
+  data
 }
 
-graphics::abline(0, 1, col = "red")
+df_dev <- simulate_data(10000)
 
+propensity_model <- glm(A ~ L, family = "binomial", data = df_dev)
+propensity_score <- predict(propensity_model, type = "response")
+iptw <- 1 / ifelse(df_dev$A == 1, propensity_score, 1 - propensity_score)
 
-lines(
-  x = cfscore$score$calplot[[1]],
-  y = cfscore$score$calplot[[2]],
-  type = "o",
-  col = "blue",
-  lwd = 2
-)
+causal_model <- glm(Y ~ P1 + P2 + A, family = "binomial", data = df_dev, weights = iptw)
+naive_model <- glm(Y ~ P1 + P2 + A, family = "binomial", data = df_dev)
+
+naive_pred <- predict_CF(naive_model, df_dev, "A", 1)
+causal_pred <- predict_CF(causal_model, df_dev, "A", 1)
+
+naive_model$coefficients
+coefficients(causal_model)
+
+riskRegression::Score(list(naive_pred, causal_pred), Y1 ~ 1, df_dev)
+
 
