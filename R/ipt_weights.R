7d7857a54742d96965f750707f22ffd6ae7492e4
Jasper van Egeraat (PHEG) <j.w.a.van_egeraat@lumc.nl>
2026-02-17 16:21:53 +0100

    dont export ipt_weights fct

--- R/ipt_weights.R
+++ R/ipt_weights.R
@@ -1,11 +1,3 @@
-#' Get the inverse probability of treatment weights
-#'
-#' @param data a data.frame
-#' @param propensity_formula a formula with on the l.h.s. the treatment variable and
-#' on the right hand side the confounders.
-#'
-#' @returns a numeric vector of the IP weights
-#' @export
 ipt_weights <- function(data, propensity_formula) {
   A <- all.vars(propensity_formula)[1]
   propensity_model <- stats::glm(propensity_formula, family = "binomial", data)
