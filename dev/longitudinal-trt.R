ffc954bdf1669fc3a671027e7c08eeb226601518
Jasper van Egeraat (PHEG) <j.w.a.van_egeraat@lumc.nl>
2026-02-18 20:06:09 +0100

    code copied from pred under interv github

---
+++ dev/longitudinal-trt.R
@@ -0,0 +1,226 @@
+library(data.table)
+
+# CFscire.long <- function(object, outcome_formula, treatment_formula)
+# treatment_formula = list(A_0 ~ L0, A1 ~ L1 + A0, ...) ?
+
+# simulate some longitudinal trt time to event data
+
+n <- 10000
+n_visits <- 5
+
+gamma_0 <- -1
+gamma_L <- 0.5
+
+alpha_0 <- -2
+alpha_A <- -0.5
+alpha_L <- 0.5
+alpha_U <- 0.5
+
+U <- rnorm(n, 0, 0.1)
+
+A <- matrix(nrow = n, ncol = n_visits)
+L <- matrix(nrow = n, ncol = n_visits)
+
+time <- rep(NA, n)
+status <- rep(NA, n)
+
+for (i in 1:n_visits) {
+
+  # simulate A and L only for those still at risk
+  ids <- which(is.na(status))
+  n_ids <- length(ids)
+
+  if (i == 1) {
+    L[ids, 1] <- rnorm(n_ids, U, 1)
+    A[ids, 1] <- rbinom(n_ids, 1, plogis(gamma_0 + gamma_L * L[ids, 1]))
+  } else {
+    L[ids, i] <- rnorm(n_ids, 0.8 * L[ids, i - 1] - A[ids, i - 1] + 0.1 * i + U[ids], 1)
+    # 0.1 * i is 0.1 * (i - 1) in Nan code
+
+    A[ids, i] <- ifelse(
+      test = A[ids, i - 1] == 1,
+      yes = 1,
+      no = rbinom(n_ids, 1, plogis(gamma_0 + gamma_L * L[ids, i]))
+    )
+  }
+
+  # do we simulate an event with current hazard before the next visit?
+  # if not, we reroll next visit
+  temp_time <- simulate_time_to_event(
+    n = n_ids,
+    constant_baseline_haz = 1,
+    LP = alpha_0 + alpha_A * A[ids, i] + alpha_L * L[ids, i] + alpha_U * U[ids]
+  )
+  time[ids] <- ifelse(
+    temp_time <= 1,
+    yes = (i - 1) + temp_time,
+    no = NA
+  )
+  status[ids] <- ifelse(
+    temp_time <= 1,
+    yes = 1,
+    no = NA
+  )
+}
+
+# censor everyone at time = 5
+time <- ifelse(is.na(time), 5, time)
+status <- ifelse(is.na(status), 0, 1)
+
+colnames(A) <- paste0("A", 1:n_visits - 1)
+colnames(L) <- paste0("L", 1:n_visits - 1)
+
+data <- data.frame(time, status, A, L)
+
+data_long <- reshape(
+  data,
+  varying = list(A = colnames(A), L = colnames(L)),
+  v.names = c("A", "L"),
+  timevar = "visit",
+  times = 1:n_visits - 1,
+  direction = "long"
+)
+
+data_long <- data_long[order(data_long$id, data_long$visit), ]
+data_long$time_start <- data_long$visit
+data_long$time_end <- pmin(data_long$time, data_long$visit + 1)
+data_long <- data_long[data_long$time_start < data_long$time, ]
+data_long$status <- ifelse(data_long$time_end == data_long$time, data_long$status, 0)
+data_long <- data_long[, c("id", "visit", "time_start", "time_end", "status", "A", "L")]
+
+setDT(data_long)
+
+data_long[, paste0("A_lag", 1:(n_visits-1)) := shift(A, n = 1:(n_visits-1), fill = 0), by = .(id)]
+data_long[, paste0("L_lag", 1:(n_visits-1)) := shift(L, n = 1:(n_visits-1), fill = 0), by = .(id)]
+data_long[, L0 := first(L), by = .(id)]
+
+# now we should form a counterfactual dataset to assess whether results are correct
+
+
+# model dev ---------------------------------------------------------------
+
+wt.mod <- glm(A ~ L, family="binomial", data=data_long[A_lag1==0,])
+pred.wt <- predict(wt.mod, type = "response", newdata = data_long)
+data_long[, wt := fifelse(A == 1, pred.wt, 1 - pred.wt)]
+data_long[, wt := fifelse(A_lag1 == 1, 1, wt)]
+data_long[, wt.cum := cumprod(wt), by = id]
+
+data_long[, ipw := 1 / wt.cum]
+
+cox.msm <- coxph(
+  Surv(time_start, time_end, status) ~ A + A_lag1 + A_lag2 + A_lag3 + A_lag4 + L0,
+  data = data_long,
+  weights = ipw
+)
+
+cumhaz <- basehaz(cox.msm, centered = FALSE)$hazard
+event.times <- basehaz(cox.msm, centered = FALSE)$time
+cumhaz.fun <- stepfun(event.times, c(0, cumhaz))
+
+L0 <- data_long[visit == 0, L0]
+
+# hazard rate term
+hrt <- function(variable, value) {
+  exp(coef(cox.msm)[variable]*value)
+}
+
+# # risk under never treated
+risk0 <- function(t) {
+  1 - exp(-cumhaz.fun(t)*hrt("L0", L0))
+}
+
+# # risk under always treated
+risk1 <- function(t) {
+  1 - exp(-(
+    cumhaz.fun(min(t, 1))*
+      hrt("L0", L0)*hrt("A", 1) +
+
+      (t >= 1) *
+      (cumhaz.fun(min(t, 2)) - cumhaz.fun(1))*
+      hrt("L0", L0)*hrt("A", 1)*hrt("A_lag1", 1) +
+
+      (t >= 2) *
+      (cumhaz.fun(min(t, 3)) - cumhaz.fun(2))*
+      hrt("L0", L0)*hrt("A", 1)*hrt("A_lag1", 1)*hrt("A_lag2", 1) +
+
+      (t >= 3) *
+      (cumhaz.fun(min(t, 4)) - cumhaz.fun(3))*
+      hrt("L0", L0)*hrt("A", 1)*hrt("A_lag1", 1)*hrt("A_lag2", 1)*hrt("A_lag3", 1) +
+
+      (t >= 4) *
+      (cumhaz.fun(min(t, 5)) - cumhaz.fun(4))*
+      hrt("L0", L0)*hrt("A", 1)*hrt("A_lag1", 1)*hrt("A_lag2", 1)*
+      hrt("A_lag3", 1)*hrt("A_lag4", 1)
+  ))
+}
+
+data_long[, `:=`(wt = NULL, wt.cum = NULL, ipw = NULL)]
+
+# 'observed' risks ----------------------------------------------------------
+
+# CF observed risk under never treatment
+
+wt.mod <- glm(A ~ L, family = "binomial", data = data_long[A_lag1 == 0])
+pred.wt0 <- predict(wt.mod, type = "response", newdata = data_long)
+data_long[, wt0 := 1 - pred.wt0]
+data_long[, ipw0 := 1/cumprod(wt0), by = id]
+data_long[, in.dat.0 := A == 0]
+
+# can this be done as weighted mean in the in.dat.0 pop?
+km.0 <- survfit(Surv(time_start, time_end, status) ~ 1,
+                data = data_long[in.dat.0 == TRUE], weights = ipw0)
+step.risk0.obs=stepfun(km.0$time,c(1,km.0$surv))
+
+risk0_obs <- 1 - step.risk0.obs(5)
+
+
+# define indicator that is 1 if treatment of interest is followed
+# means subject must follow treatment strategy of interest until survtime or
+# until prediction horizon
+data_long[, correct_treatment := all(as.integer(A == 0)), by = .(id)] # works in this simple case
+data_wide <- data_long[, .SD[.N], by = id]
+data_wide[, risk_untreated := risk0(5)]
+
+brier_null <- data_wide[, cf_brier(
+  obs_outcome = data_wide$status,
+  obs_trt = correct_treatment,
+  cf_pred = rep(risk0_obs, 10000),
+  cf_trt = 1,
+  ipw = ipw0
+)]
+
+(brier_null - 0.224619)/brier_null*100
+
+lapply(
+  X = c("auc", "brier", "oeratio"),
+  FUN = function(m) {
+    data_wide[, cf_metric(metric = m,
+                          obs_outcome = data_wide$status,
+                          obs_trt = correct_treatment,
+                          cf_pred = risk_untreated,
+                          cf_trt = 1,
+                          ipw = ipw0
+    )]
+  }
+)
+
+
+# data_new <- data_long[, .(L0 = first(L)), by = id]
+
+
+# make wide again (until time horizon of interest)
+
+time_horizon <- 5
+
+
+
+
+
+
+data_long[time_end <= time_horizon, .SD[.N], by = id]
+
+
+
+test <- data.table(A = c(0, 0, 1, 0, 0, 1))
+
+
