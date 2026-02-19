0b0866663e1e0e5ece346ebe1ccc3de34396e5ab
Jasper van Egeraat <j.w.a.van_egeraat@lumc.nl>
2026-02-19 14:03:34 +0100

    make long in fct

--- dev/longitudinal-trt.R
+++ dev/longitudinal-trt.R
@@ -38,7 +38,7 @@ simulate_longitudinal <- function(n, fix_trt = NULL) {
 
   for (i in 2:n_visits) {
     L[, i] <- rnorm(n, plogis(0.8 * L[, i - 1] - A[, i - 1] + 0.1 * i + U))
-    A[, i] <- ifelse(A[, i - 1] == 1, 1, simulate_A(i, L, fix_trt))
+    A[, i] <- ifelse(A[, i - 1] == 1, rep(1, n), simulate_A(i, L, fix_trt))
   }
 
   for (i in 1:n_visits){
@@ -55,53 +55,59 @@ simulate_longitudinal <- function(n, fix_trt = NULL) {
   colnames(A) <- paste0("A", 1:n_visits - 1)
   colnames(L) <- paste0("L", 1:n_visits - 1)
 
-  data.table(time, status, A, L, U)
+  data.table(id = 1:n, time, status, A, L, U)
 }
 
 df_dev <- simulate_longitudinal(n)
+summary(df_dev$time)
+
 df_cf0 <- simulate_longitudinal(n, 0)
 df_cf1 <- simulate_longitudinal(n, 1)
 
+make_long <- function(data) {
+  long <- melt(data,
+       measure.vars = patterns("^A", "^L"),
+       variable.name = "visit",
+       value.name = c("A", "L"),
+       variable.factor = F)
+
+  long[, visit := as.numeric(visit) - 1]
+
+  long <- long[order(id, visit)]
+  long[, `:=`(
+    time_start = visit,
+    time_end = pmin(time, visit + 1)
+  )]
+  long <- long[time_start < time, ]
+  long[, status := fifelse(time_end == time, status, 0)]
+  long[, time := NULL]
+
+  long[, paste0("A_lag", 1:(n_visits-1)) := shift(A, n = 1:(n_visits-1), fill = 0), by = .(id)]
+  long[, paste0("L_lag", 1:(n_visits-1)) := shift(L, n = 1:(n_visits-1), fill = 0), by = .(id)]
+  long[, L0 := first(L), by = .(id)]
+
+  return(long[])
+}
+
+df_dev_long <- make_long(df_dev)
 
 
-data_long <- reshape(
-  data,
-  varying = list(A = colnames(A), L = colnames(L)),
-  v.names = c("A", "L"),
-  timevar = "visit",
-  times = 1:n_visits - 1,
-  direction = "long"
-)
-
-data_long <- data_long[order(data_long$id, data_long$visit), ]
-data_long$time_start <- data_long$visit
-data_long$time_end <- pmin(data_long$time, data_long$visit + 1)
-data_long <- data_long[data_long$time_start < data_long$time, ]
-data_long$status <- ifelse(data_long$time_end == data_long$time, data_long$status, 0)
-data_long <- data_long[, c("id", "visit", "time_start", "time_end", "status", "A", "L")]
-
-setDT(data_long)
-
-data_long[, paste0("A_lag", 1:(n_visits-1)) := shift(A, n = 1:(n_visits-1), fill = 0), by = .(id)]
-data_long[, paste0("L_lag", 1:(n_visits-1)) := shift(L, n = 1:(n_visits-1), fill = 0), by = .(id)]
-data_long[, L0 := first(L), by = .(id)]
 
 # now we should form a counterfactual dataset to assess whether results are correct
 
-
 # model dev ---------------------------------------------------------------
 
-wt.mod <- glm(A ~ L, family="binomial", data=data_long[A_lag1==0,])
-pred.wt <- predict(wt.mod, type = "response", newdata = data_long)
-data_long[, wt := fifelse(A == 1, pred.wt, 1 - pred.wt)]
-data_long[, wt := fifelse(A_lag1 == 1, 1, wt)]
-data_long[, wt.cum := cumprod(wt), by = id]
+wt.mod <- glm(A ~ L, family="binomial", data=df_dev_long[A_lag1==0,])
+pred.wt <- predict(wt.mod, type = "response", newdata = df_dev_long)
+df_dev_long[, wt := fifelse(A == 1, pred.wt, 1 - pred.wt)]
+df_dev_long[, wt := fifelse(A_lag1 == 1, 1, wt)]
+df_dev_long[, wt.cum := cumprod(wt), by = id]
 
-data_long[, ipw := 1 / wt.cum]
+df_dev_long[, ipw := 1 / wt.cum]
 
 cox.msm <- coxph(
   Surv(time_start, time_end, status) ~ A + A_lag1 + A_lag2 + A_lag3 + A_lag4 + L0,
-  data = data_long,
+  data = df_dev_long,
   weights = ipw
 )
 
@@ -109,7 +115,7 @@ cumhaz <- basehaz(cox.msm, centered = FALSE)$hazard
 event.times <- basehaz(cox.msm, centered = FALSE)$time
 cumhaz.fun <- stepfun(event.times, c(0, cumhaz))
 
-L0 <- data_long[visit == 0, L0]
+L0 <- df_dev_long[visit == 0, L0]
 
 # hazard rate term
 hrt <- function(variable, value) {
@@ -146,21 +152,21 @@ risk1 <- function(t) {
   ))
 }
 
-data_long[, `:=`(wt = NULL, wt.cum = NULL, ipw = NULL)]
+df_dev_long[, `:=`(wt = NULL, wt.cum = NULL, ipw = NULL)]
 
 # 'observed' risks ----------------------------------------------------------
 
 # CF observed risk under never treatment
 
-wt.mod <- glm(A ~ L, family = "binomial", data = data_long[A_lag1 == 0])
-pred.wt0 <- predict(wt.mod, type = "response", newdata = data_long)
-data_long[, wt0 := 1 - pred.wt0]
-data_long[, ipw0 := 1/cumprod(wt0), by = id]
-data_long[, in.dat.0 := A == 0]
+wt.mod <- glm(A ~ L, family = "binomial", data = df_dev_long[A_lag1 == 0])
+pred.wt0 <- predict(wt.mod, type = "response", newdata = df_dev_long)
+df_dev_long[, wt0 := 1 - pred.wt0]
+df_dev_long[, ipw0 := 1/cumprod(wt0), by = id]
+df_dev_long[, in.dat.0 := A == 0]
 
 # can this be done as weighted mean in the in.dat.0 pop?
 km.0 <- survfit(Surv(time_start, time_end, status) ~ 1,
-                data = data_long[in.dat.0 == TRUE], weights = ipw0)
+                data = df_dev_long[in.dat.0 == TRUE], weights = ipw0)
 step.risk0.obs=stepfun(km.0$time,c(1,km.0$surv))
 
 risk0_obs <- 1 - step.risk0.obs(5)
@@ -169,19 +175,19 @@ risk0_obs <- 1 - step.risk0.obs(5)
 # define indicator that is 1 if treatment of interest is followed
 # means subject must follow treatment strategy of interest until survtime or
 # until prediction horizon
-data_long[, correct_treatment := all(as.integer(A == 0)), by = .(id)] # works in this simple case
-data_wide <- data_long[, .SD[.N], by = id]
+df_dev_long[, correct_treatment := all(as.integer(A == 0)), by = .(id)] # works in this simple case
+data_wide <- df_dev_long[, .SD[.N], by = id]
 data_wide[, risk_untreated := risk0(5)]
 
-brier_null <- data_wide[, cf_brier(
-  obs_outcome = data_wide$status,
-  obs_trt = correct_treatment,
-  cf_pred = rep(risk0_obs, 10000),
-  cf_trt = 1,
-  ipw = ipw0
-)]
-
-(brier_null - 0.224619)/brier_null*100
+# brier_null <- data_wide[, cf_brier(
+#   obs_outcome = data_wide$status,
+#   obs_trt = correct_treatment,
+#   cf_pred = rep(risk0_obs, 10000),
+#   cf_trt = 1,
+#   ipw = ipw0
+# )]
+#
+# (brier_null - 0.224619)/brier_null*100
 
 lapply(
   X = c("auc", "brier", "oeratio"),
@@ -197,22 +203,5 @@ lapply(
 )
 
 
-# data_new <- data_long[, .(L0 = first(L)), by = id]
-
-
-# make wide again (until time horizon of interest)
-
-time_horizon <- 5
-
-
-
-
-
-
-data_long[time_end <= time_horizon, .SD[.N], by = id]
-
-
-
-test <- data.table(A = c(0, 0, 1, 0, 0, 1))
 
 
